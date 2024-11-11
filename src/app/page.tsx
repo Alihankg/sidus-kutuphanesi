faf1e6d41dd556d790dad1169530eff0ff95d1aa
alikhan <kebrun2017@gmail.com>
2024-11-11 17:22:20 +0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,40 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+'use client'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<div className="flex items-center justify-center h-dvh bg-[url('/img/library-bg.jpg')] bg-center bg-cover bg-opacity-75">
+			<div className="card bg-opacity-70 bg-black p-5 rounded-md min-w-80 w-[50%] flex flex-col gap-2 justify-center items-center">
+				<p className="text-3xl lg:text-2xl text-center">
+					<span className="font-bold bg-gradient-to-br from-[#DF3AD9] to-[#F67B6B] bg-clip-text text-transparent">
+						Sidus Kütüphanesi
+					</span>
+					&apos;ne hoşgeldin!
+				</p>
+				<p className="text-2xl lg:text-xl">
+					Burada çeşitli ve tatlı karakterlerle evrenin sırlarını
+					keşfedebileceksin!
+				</p>
+				<img
+					src="/img/schro2.png"
+					alt="schrodinger"
+					className="w-96 animate-stretchShrink origin-bottom"
+				/>
+				<div
+					className="bg-gradient-to-br from-[#696EFF] to-[#F8ACFF] text-gray-800 rounded-md px-4 py-2 hover:cursor-pointer"
+					onClick={() => console.log('start')}>
+					Hemen Başla!
+				</div>
+			</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+			<div className="absolute bottom-10 right-10">
+				Image by{' '}
+				<a
+					className=" underline hover:text-blue-500"
+					href="https://www.freepik.com/free-vector/abandoned-library-with-old-books-dusty-shelves-working-desks-with-lamps-vector-cartoon-illustration-large-room-with-bookshelves-walls-covered-with-cobweb-haunted-archive-interior_69439840.htm#fromView=keyword&page=1&position=1&uuid=1b982509-1035-49f2-9a9b-1cc0fcf7e15b">
+					Freepik
+				</a>
+			</div>
+		</div>
+	)
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+			keyframes: {
+				stretchShrink: {
+					'0%, 100%': { transform: 'scaleY(1)' }, // Normal size
+					'50%': { transform: 'scaleY(1.05)' }, // Stretched size
+				},
+			},
+			animation: {
+				stretchShrink: 'stretchShrink 5s ease-in-out infinite',
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config
